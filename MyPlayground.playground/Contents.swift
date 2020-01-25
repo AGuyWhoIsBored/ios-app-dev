f91aad6547bed8199042182a16193cb293b0f270
LibLabs-Mac <liblabs-mac@lib-216a-06.ad.calpoly.edu>
2020-01-25 15:04:41 -0800

    testing git with a playground

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+var str = "Hello, playground"
+print("This is a dub")
+for i in [0, 1, 2, 3, 4, 5]
+{
+    print(String(i) + " dubs")
+}
+// this is just some test code to test the repository!
