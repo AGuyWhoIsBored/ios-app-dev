b27357f11bdefb611258e3149145410c0bfc1e48
praptor101 <prabdilsandhu707@gmail>
2020-02-08 13:58:35 -0800

    Tutorial Time

---
+++ Motivational Tutorial App/Motivational Tutorial App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Motivational Tutorial App
+//
+//  Created by LibLabs-Mac on 2/8/20.
+//  Copyright © 2020 CPES. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
